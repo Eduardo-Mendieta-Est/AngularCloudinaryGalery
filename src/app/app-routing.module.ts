e3bf6e4d0f2083eea38144dc0b29cd5f70cb7a43
Eduardo Mendieta <chrisseduu13mc@gmail.com>
2021-04-11 18:49:44 -0500

    subiendo proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetalleComponent } from './imagen/detalle.component';
+import { ListaComponent } from './imagen/lista.component';
+import { NuevaComponent } from './imagen/nueva.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ListaComponent},
+  {path: 'nueva', component: NuevaComponent},
+  {path: 'detalle', component: DetalleComponent},
+  {path:'**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
